44b2345b07d201468cd7c35ea708c61f73a65825
v1n6ane <vinciane.bizet@gmail.com>
2017-12-01 19:22:09 +0100

    Suppression dossier source

--- source/othello-js-master/random-ai.js
+++
@@ -1,6 +0,0 @@
-othello.registerAI({
-  findTheBestMove: function (gameTree) {
-    return gameTree.moves[Math.floor(Math.random() * gameTree.moves.length)];
-  }
-});
-// vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
